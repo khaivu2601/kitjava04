fbd9e1de0577b6a6295a47389c03632857f8fea2
VŨ XUÂN KHẢI <66871042+khaivu2601@users.noreply.github.com>
2020-09-21 19:41:02 +0700

    buổi 4

---
+++ btBANHANg.java
@@ -0,0 +1,10 @@
+package buoi4.nha;
+
+public class btBANHANg {
+    public static void main(String[] args) {
+        BANHANG sp1=new BANHANG();
+        sp1.nhapThongtin();
+        sp1.laiXuat();
+        sp1.laiXuat2();
+    }
+}
